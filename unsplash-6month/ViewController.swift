2deaf140ccd5086f1e1674b0259de4eab8a8cd0c
telegeyN <ntelegey@gmail.com>
2024-08-15 12:19:49 +0600

    Initial Commit

---
+++ unsplash-6month/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unsplash-6month
+//
+//  Created by Telegey Nurbekova on 15/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
